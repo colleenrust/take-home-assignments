a92e2c803450f8ec4963ee6f61a2396768bfc858
Colleen <colleenrust524@EMAIL.com>
2023-05-23 15:56:57 -0500

    accomplished little but made the new files for the simplified blackjack and maze questions. stupid commit

---
+++ maze.rb
@@ -0,0 +1,20 @@
+# Solve a real world problem using computer science data structures and algorithms
+# Mazes
+
+# Imagine a rectangular piece of graph paper. We could create a maze by coloring some of the lines to make walls. 
+
+# We call that maze well-formed if it has exactly two exterior openings (an entrance and an exit), and exactly one path that connects the two openings. 
+
+# 1. Design a data structure that can represent a rectangular maze. 
+# 2. Create an algorithm that determines whether the maze is well-formed. 
+# 3. Report the solution if the maze is well-formed.
+class Maze
+  def initialize
+
+  end
+
+end
+
+
+
+# fuck this stupid problem
